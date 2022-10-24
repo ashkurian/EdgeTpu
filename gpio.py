24c19302762ea8bb36844f932619d353b07ac35a
ashkurian <116532854+ashkurian@users.noreply.github.com>
2022-10-23 22:25:17 -0400

    Create gpio.py

---
+++ gpio.py
@@ -0,0 +1,15 @@
+from periphery import GPIO
+import time
+
+led = GPIO("/dev/gpiochip0", 39, "out")  # pin 40
+# button = GPIO("/dev/gpiochip4", 13, "in")  # pin 36
+
+try:
+  while True:
+    led.write(True)
+    time.sleep(2)
+    led.write(False)
+    time.sleep(2)
+finally:
+  led.write(False)
+  led.close(
