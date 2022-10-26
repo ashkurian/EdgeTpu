53484d8c71f7f6ec4b99ed01f79d8ab68050a443
ashkurian <116532854+ashkurian@users.noreply.github.com>
2022-10-26 17:52:12 -0400

    Update code_study.py

--- code_study.py
+++ code_study.py
@@ -1,19 +1,3 @@
-# EdgeTpu
-# EdgeTpu
-# Lint as: python3
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Example using PyCoral to classify a given image using an Edge TPU.
 To run this code, you must attach an Edge TPU attached to the host and
 install the Edge TPU runtime (`libedgetpu.so`) and `tflite_runtime`. For
@@ -28,19 +12,14 @@ python3 classify-image.py \
 
 import argparse
 import time
-#from periphery import GPIO
+from periphery import GPIO, Serial
 import numpy
 
 from PIL import Image
-
-#from pycoral.adapters import classify
-import classify
-#from pycoral.adapters 
-import common
-#from pycoral.utils.dataset 
-
-#import read_label_file
-#from pycoral.utils.edgetpu import make_interpreter
+from pycoral.adapters import classify
+from pycoral.adapters import common
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
 
 
 def main():
@@ -60,11 +39,13 @@ def main():
                       help='Number of times to run inference')
   args = parser.parse_args()
 
-  labels = read_label_file(args.labels) if args.labels else {}
+  labels = read_label_file(args.labels) if args.labels else {}  #reading the label file generated
 
-  interpreter = make_interpreter(*args.model.split('@'))
+  interpreter = make_interpreter(*args.model.split('@'))  # Models obtained from TfLiteConverter is run in Python with Interpreter.
   interpreter.allocate_tensors()
+  ## tflite_model can be saved to a file and loaded later, or directly into the Interpreter. 
+  ##Since TensorFlow Lite pre-plans tensor allocations to optimize inference, the user needs to call allocate_tensors() before any inference.
 
-  _, height, width = interpreter.get_input_details()[0]['shape']
-  size = [height, width]
+  _, height, width = interpreter.get_input_details()[0]['shape'] 
+  size = [height, width] # getting the size of the input 
   print(size)
