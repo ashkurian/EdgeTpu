e1c49c6ef5d199f7aa349b186cfb156b280f30f1
ashkurian <116532854+ashkurian@users.noreply.github.com>
2022-10-26 17:47:45 -0400

    Update code_study.py

--- code_study.py
+++ code_study.py
@@ -1,1 +1,70 @@
 # EdgeTpu
+# EdgeTpu
+# Lint as: python3
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Example using PyCoral to classify a given image using an Edge TPU.
+To run this code, you must attach an Edge TPU attached to the host and
+install the Edge TPU runtime (`libedgetpu.so`) and `tflite_runtime`. For
+device setup instructions, see coral.ai/docs/setup.
+Example usage:
+```
+python3 classify-image.py \
+  --model mnist_model_quant_edgetpu.tflite  \
+  --labels mnist_labels.txt \
+```
+"""
+
+import argparse
+import time
+#from periphery import GPIO
+import numpy
+
+from PIL import Image
+
+#from pycoral.adapters import classify
+import classify
+#from pycoral.adapters 
+import common
+#from pycoral.utils.dataset 
+
+#import read_label_file
+#from pycoral.utils.edgetpu import make_interpreter
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('-m', '--model', required=True,
+                      help='File path of .tflite file.')
+  parser.add_argument('-i', '--input',
+                      help='Image to be classified.')
+  parser.add_argument('-l', '--labels',
+                      help='File path of labels file.')
+  parser.add_argument('-k', '--top_k', type=int, default=1,
+                      help='Max number of classification results')
+  parser.add_argument('-t', '--threshold', type=float, default=0.0,
+                      help='Classification score threshold')
+  parser.add_argument('-c', '--count', type=int, default=5,
+                      help='Number of times to run inference')
+  args = parser.parse_args()
+
+  labels = read_label_file(args.labels) if args.labels else {}
+
+  interpreter = make_interpreter(*args.model.split('@'))
+  interpreter.allocate_tensors()
+
+  _, height, width = interpreter.get_input_details()[0]['shape']
+  size = [height, width]
+  print(size)
