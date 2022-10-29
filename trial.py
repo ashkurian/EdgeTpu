72d9e00afeb276dcc8fa7a9a3d29fecbbde41098
ashkurian <116532854+ashkurian@users.noreply.github.com>
2022-10-29 16:59:29 -0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -84,7 +84,8 @@ def main():
     #output_tensor = interpreter.get_tensor(1)[0]
     output_tensor = interpreter.get_tensor(output_details['index'])[0]
     print(output_tensor[1])
-    uart1.write(output_tensor[1].tobytes())
+    #uart1.write(output_tensor[1].tobytes())
+    uart1.write(output_tensor[1])
     
     print('%.6fms' % (inference_time * 1000))
     
