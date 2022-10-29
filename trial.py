ad1e2f3c0446f6d7c6692a88815d14942a64f10e
ashkurian <116532854+ashkurian@users.noreply.github.com>
2022-10-29 15:43:19 -0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -66,10 +66,10 @@ def main():
     print(list(arr))
     arr = numpy.array(list(arr), dtype='uint8')
     arr = numpy.reshape(arr, (28,28))
-    #image = Image.fromarray(arr, 'L').resize(size, Image.ANTIALIAS)
+    image = Image.fromarray(arr, 'L').resize(size, Image.ANTIALIAS)
    
     #common.set_input(interpreter, image)
-    #interpreter.set_tensor(input_details['index'], arr)
+    #interpreter.set_tensor(input_details['index'], image)
     self.interpreter.set_tensor(input_details['index'], arr)[0]
     #inspector_start = int.from_bytes(uart3.read(1, 1), 'big')
     #print("read {:d} bytes: _{:s}_".format(len(inspector_start), inspector_start))
