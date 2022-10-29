4bda4672b1c72c48c08991879013fe648c53f046
ashkurian <116532854+ashkurian@users.noreply.github.com>
2022-10-29 17:41:13 -0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -84,7 +84,7 @@ def main():
     #output_tensor = interpreter.get_tensor(1)[0]
     output_tensor = interpreter.get_tensor(output_details['index'])[0]
     print(output_tensor[1])
-    output_tensor[1]=output_tensor[1].to_bytes(1, 'big')
+    output_tensor[1]=output_tensor[1].tobytes()
     print(output_tensor[1])
     
     #print(list(numpy.array(output_tensor[1])).tobytes())
