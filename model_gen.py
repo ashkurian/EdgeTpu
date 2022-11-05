f13f9aa29b80358f73207009933685f2c03b6e35
ashkurian <116532854+ashkurian@users.noreply.github.com>
2022-11-05 19:24:58 -0400

    Update model_gen.py

--- model_gen.py
+++ model_gen.py
@@ -1,7 +1,7 @@
-# EdgeTpu
 import logging
 logging.getLogger("tensorflow").setLevel(logging.DEBUG)
 
+import pathlib
 
 import numpy as np
 
@@ -16,8 +16,8 @@ import matplotlib.pyplot as plt
 import random
 
 print(tf.__version__)
-input_size = 783
-node_size = 100
+input_size = 784
+node_size = 500
 # Load MNIST dataset
 mnist = tf.keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -30,10 +30,61 @@ train_images = train_images.astype(np.float32)
 test_images = test_images.astype(np.float32) 
 # Define the model architecture
 model = tf.keras.Sequential([
-  tf.keras.layers.InputLayer(input_shape=(input_size)),
+  #tf.keras.layers.InputLayer(input_shape=(input_size)),
+  tf.keras.layers.InputLayer(input_shape=(28,28)),
+  #tf.keras.layers.Reshape(target_shape=(28, 28, 1)),
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(node_size)
+  # tf.keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation='relu'),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+  # tf.keras.layers.Dense(1),
+
+  #tf.keras.layers.Dense(node_size)
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+  tf.keras.layers.Dense(500),
+
+  # tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+  # tf.keras.layers.Flatten(),
+  # tf.keras.layers.Dense(10)
 ])
+
 
 
 
@@ -48,26 +99,36 @@ model.compile(optimizer='adam',
   #  epochs=5,
    #validation_data=(test_images, test_labels)
 # )
-
-
-###setting weights
+model.summary()
+#w,b = layer1.get_weights()
 w = np.zeros((input_size,node_size),dtype='int8')
 for i in range(input_size):
-  for j in range(50):
+  for j in range(node_size):
     w[i][j] = 97
-  for j in range(50,100):
-    w[i][j] = 41
+  
     
 b = np.zeros((node_size,),dtype='int8')
 for i in range(node_size):
   b[i] = 26
 wb = [w,b]
-wb
-model.set_weights(wb)
-model.get_weights()
+#wb
+#784 500 configuration for wb
+
+model.layers[1].set_weights(wb)
 
 
-###integer quantization 
+
+w1 = np.zeros((node_size,node_size),dtype='int8')
+for i in range(node_size):
+  for j in range(node_size):
+    w1[i][j] = 97
+  
+wb1 = [w1,b]
+for i in range(19):  #19 layers with 500 500 configuration for w1 
+  model.layers[i+2].set_weights(wb1)
+  #model.layers[i+1].get_weights()
+
+
 
 def representative_data_gen():
   for input_value in tf.data.Dataset.from_tensor_slices(train_images).batch(1).take(100):
@@ -78,22 +139,32 @@ converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.representative_dataset = representative_data_gen
 # Ensure that if any ops can't be quantized, the converter throws an error
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-# For full integer quantization, though supported types defaults to int8 only, we explicitly declare it for clarity.
+##
 converter.target_spec.supported_types = [tf.int8]
 # Set the input and output tensors to uint8 (APIs added in r2.3)
 converter.inference_input_type = tf.uint8
 converter.inference_output_type = tf.uint8
-#### generating the tflite file
+
 tflite_model_quant = converter.convert()
 
+#interpreter = tf.lite.Interpreter(model_content=tflite_model_quant)
+#input_type = interpreter.get_input_details()[0]['dtype']
+#print('input: ', input_type)
+#output_type = interpreter.get_output_details()[0]['dtype']
+#print('output: ', output_type)
+#interpreter.get_tensor_details()
+#tflite_model_quant_file = 20L500Nmodified.tflite
+#20L500Nmodified.write_bytes(tflite_model_quant)
 
-################# downloading the generated .tflite file 
-with open('1L64N.tflite', 'wb') as f:
-  f.write(tflite_model_quant)
-  # Save the quantized model to file to the Downloads directory
-# Download the digit classification model
+
+tflite_models_dir = pathlib.Path("/tmp/mnist_tflite_models/")
+tflite_models_dir.mkdir(exist_ok=True, parents=True)
+
+# Save the quantized model:
+tflite_model_quant_file = tflite_models_dir/"20L500N_mod_mnist_model_quant.tflite"
+tflite_model_quant_file.write_bytes(tflite_model_quant)
+#tflite_model_quant
 from google.colab import files
-files.download('1L64N.tflite')
+files.download(tflite_model_quant_file)
 
-print('`1L64N.tflite` has been downloaded')
-
+print('`20L500N_mod_mnist_model_quant.tflite` has been downloaded')
